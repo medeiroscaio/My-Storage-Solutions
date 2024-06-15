cf1a94331318b4ce137e84390142f7da0bf04444
Yuri Diego <contatoprofissional.ya@gmail.com>
2024-06-15 16:38:10 -0300

    Added Javascript to feedback for select item on table...

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,18 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const rows = document.querySelectorAll('.table-body tr');
-    
+    const buttons = document.querySelectorAll('.buttons-section button');
+
     rows.forEach(row => {
         row.addEventListener('click', () => {
             if (row.classList.contains('selected')) {
                 row.classList.remove('selected');
+                // Remove a classe de estilo dos botões se a linha for desselecionada
+                buttons.forEach(button => button.classList.remove('selected-row'));
             } else {
                 rows.forEach(r => r.classList.remove('selected'));
                 row.classList.add('selected');
+                // Adiciona a classe de estilo aos botões quando uma linha é selecionada
+                buttons.forEach(button => button.classList.add('selected-row'));
             }
         });
     });
